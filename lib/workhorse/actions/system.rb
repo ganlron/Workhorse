41cdc90faa289b68d562519d0d849ac0a8fa8382
Derek Buttineau <derek@csolve.net>
2010-05-05 11:40:21 -0400

    Added in dynamic method definitions to the system action as an example

--- lib/workhorse/actions/system.rb
+++ lib/workhorse/actions/system.rb
@@ -4,10 +4,33 @@ require 'eventmachine'
 module Workhorse
   module Actions
     class System
+      mattr_accessor :methods
       include EM::Deferrable
+      
+      @@methods = ["uptime", "hardware", "kernel", "hostname", "os"]
 
       def uptime
         res = `uptime`
+        set_deferred_status :succeeded, res
+      end
+      
+      def hardware
+        res = `uname -m`
+        set_deferred_status :succeeded, res
+      end
+      
+      def kernel
+        res = `uname -i`
+        set_deferred_status :succeeded, res
+      end
+      
+      def hostname
+        res = `uname -n`
+        set_deferred_status :succeeded, res
+      end
+      
+      def os
+        res = `uname -sr`
         set_deferred_status :succeeded, res
       end
 
@@ -19,17 +42,19 @@ module Workhorse
   module Actions
     class SystemHandler
       include WH::Actions::Handler
-
-      def handle_uptime
-        EM.spawn do
-          whsys = WH::Actions::System.new
-          whsys.callback do |val|
-            WH.reply(@@message, val, @@muc)
-          end
-          whsys.uptime
-        end.notify
-      end
       
+      WH::Actions::System.methods.each do |m|
+        define_method "handle_#{m}".to_sym do
+          EM.spawn do
+            whsys = WH::Actions::System.new
+            whsys.callback do |val|
+              WH.reply(@@message, val, @@muc)
+            end
+            whsys.send(m.to_sym)
+          end.notify
+        end
+      end
+
     end
   end
 end
